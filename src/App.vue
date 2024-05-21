b05da09252af8303ac9067fdbd0a0c111d8bd28f
jacob <jacobgraham@woosterbrush.com>
2024-05-21 09:36:06 -0400

    add event emitters and listeners for all paperjs canvas events

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,21 @@ import BottomToolbar from './components/BottomToolbar.vue'
 <div>
   <div class="grid grid-cols-4 grid-rows-4">
     <SideToolbar class="row-span-3 col-span-1"/>
-    <DrawingCanvas class="row-span-3 col-span-3"/>
+    <DrawingCanvas class="row-span-3 col-span-3"
+      @mouseDown="console.log('mouse down')"
+      @mouseDrag="console.log('mouse drag')"
+      @mouseUp="console.log('mouse up')"
+      @mouseMove="console.log('mouse move')"
+      @mouseLeave="console.log('mouse leave')"
+      @mouseEnter="console.log('mouse enter')"
+      @mouseWheel="console.log('mouse wheel')"
+      @keyDown="console.log('key down')"
+      @keyUp="console.log('key up')"
+      @keyPress="console.log('key press')"
+      @frame="console.log('frame')"
+      @resize="console.log('resize')"
+      @scroll="console.log('scroll')"
+    />
     <BottomToolbar class="row-span-1 col-span-4 bg-green-400"/>
   </div>
 </div>
--- src/components/DrawingCanvas.vue
+++ src/components/DrawingCanvas.vue
@@ -5,16 +5,98 @@
 </template>
   
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, defineEmits } from 'vue'
 import paper from 'paper'
 
 //references to ui elements
 const canvas = ref(null)
 
+//define events that can be emitted
+const emit = defineEmits([
+    'mouseDown',
+    'mouseDrag', 
+    'mouseUp', 
+    'mouseMove', 
+    'mouseLeave', 
+    'mouseEnter', 
+    'mouseWheel', 
+    'keyDown', 
+    'keyUp', 
+    'keyPress', 
+    'frame', 
+    'resize', 
+    'scroll'
+])
+
 //initialize paper.js
 onMounted(() => {
     //init paper
     paper.setup(canvas.value)
+
+    //emit event when mouse is pressed
+    paper.view.onMouseDown = (event) => {
+        emit('mouseDown', event)
+    }
+
+    //emit event when mouse is dragged
+    paper.view.onMouseDrag = (event) => {
+        emit('mouseDrag', event)
+    }
+
+    //emit event when mouse is released
+    paper.view.onMouseUp = (event) => {
+        emit('mouseUp', event)
+    }
+
+    //emit event when mouse is moved
+    paper.view.onMouseMove = (event) => {
+        emit('mouseMove', event)
+    }
+    
+    //emit event when mouse leaves the canvas
+    paper.view.onMouseLeave = (event) => {
+        emit('mouseLeave', event)
+    }
+
+    //emit event when mouse enters the canvas
+    paper.view.onMouseEnter = (event) => {
+        emit('mouseEnter', event)
+    }
+
+    //emit event when mouse wheel is scrolled
+    paper.view.onMouseWheel = (event) => {
+        emit('mouseWheel', event)
+    }
+
+    //emit event when key is pressed
+    paper.view.onKeyDown = (event) => {
+        emit('keyDown', event)
+    }
+
+    //emit event when key is released
+    paper.view.onKeyUp = (event) => {
+        emit('keyUp', event)
+    }
+
+    //emit event when key is pressed
+    paper.view.onKeyPress = (event) => {
+        emit('keyPress', event)
+    }
+
+    //emit event when frame is rendered
+    paper.view.onFrame = (event) => {
+        emit('frame', event)
+    }
+
+    //emit event when canvas is resized
+    paper.view.onResize = (event) => {
+        emit('resize', event)
+    }
+
+    //emit event when canvas is scrolled
+    paper.view.onScroll = (event) => {
+        emit('scroll', event)
+    }
 
     //draw a circle
     //eslint-disable-next-line
