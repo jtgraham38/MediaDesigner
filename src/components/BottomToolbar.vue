644a72a9e0611f8634c66b75804b491054c2568b
jacob <jacobgraham@woosterbrush.com>
2024-05-22 10:35:10 -0400

    Added my custom styled buttons!

--- src/components/BottomToolbar.vue
+++ src/components/BottomToolbar.vue
@@ -1,5 +1,19 @@
 <template>
-    <div class="row-span-2 col-span-1 bg-green-400"></div>
+    <div class="bg-green-400 z-40">
+        <br>
+        <br>
+        <br>
+        <br>
+        <span class="m-4"></span>
+        <button class="bttn">Button</button>
+
+        <span class="m-4"></span>
+        <button popovertarget="popover" class=" btn btn-primary">Tailwind</button>
+        <span class="m-4"></span>
+        <button class="btn btn-secondary">Tailwind</button>
+        <div popover id="popover" class="p-2 border">it workd!</div>
+    </div>
+    
 </template>
   
 <script setup>
