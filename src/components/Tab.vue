a1d03f121088015f48a5ae32da86b8685d64b6ec
jacob <jacobgraham@woosterbrush.com>
2024-05-23 13:09:04 -0400

    add a basic vertical tab bar system.

---
+++ src/components/Tab.vue
@@ -0,0 +1,32 @@
+<template>
+<div @click="handleClick" class="p-6 text-xl hover:bg-highlight-900 text-primary-300 hover:text-primary-400 flex space-x-2 border-b-2 border-primary-400 hover:border-primary-500">
+    <slot></slot>
+</div>
+</template>
+    
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+//define props
+const props = defineProps({
+    tabId: {
+        type: String,
+        required: true,
+    }
+})
+
+//define emits
+const emit = defineEmits(['tabChanged']);
+
+//handle click event
+const handleClick = () => {
+    emit('tabChanged', props.tabId);
+}
+
+</script>
+    
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/components/VerticalTabs.vue
@@ -0,0 +1,28 @@
+<template>   
+    <div class="bg-highlight-600 ">
+        <div class="flex space-x-2 h-full">
+            <div class="flex-noneflex flex-col h-full bg-highlight-800">
+                <slot name="tabs">
+                    
+                </slot>
+            </div>
+            <div class="p-2 h-full w-full overflow-y-auto text-primary-200">
+                <slot name="content">
+                    
+                </slot>
+            </div>
+        </div>
+    </div>
+    
+</template>
+  
+<script setup>
+
+
+</script>
+  
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+  
